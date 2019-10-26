417052e917260344a2d5740e356bacf8f78a2384
dugaszadam <dugasz.adam@gmail.com>
2019-10-26 09:40:03 +0200

    Working top 2 buttons, working map, working reservation menu, ready reservation gui

--- app/src/main/java/com/example/myapplication/ReservActivity.java
+++ app/src/main/java/com/example/myapplication/ReservActivity.java
@@ -1,22 +1,80 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
-public class ReservActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ReservActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText selectDate,selectTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserv);
+        selectDate=findViewById(R.id.date);
+        selectTime=findViewById(R.id.time);
+
+        selectDate.setOnClickListener(this);
+        selectTime.setOnClickListener(this);
+
+
+
+
     }
 
+    @Override
+    public void onClick(View view) {
+
+        if (view == selectDate) {
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
 
 
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            selectDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if (view == selectTime) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            selectTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
+    }
 }
