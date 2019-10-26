d0912d91afa888509d3addb5c82a158ee2c14c7f
Tóth Martin <tothmartinofficial@gmail.com>
2019-10-26 02:26:15 +0200

    Publish added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,25 +9,30 @@ import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    String clientId = MqttClient.generateClientId();
+    String clientId;
+    MqttAndroidClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MqttAndroidClient client = new MqttAndroidClient(MainActivity.this, "tcp://100.98.11.17:1883",
-                        clientId);
+        clientId = "kaki"; //MqttClient.generateClientId()
+        client = new MqttAndroidClient(MainActivity.this, "tcp://100.98.11.17:1883", clientId);
 
         try {
             client.connect().setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+                    publish();
                 }
 
                 @Override
@@ -39,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
         } catch (MqttException e) {
             e.printStackTrace();
         }
+    }
 
+    void publish() {
+        MqttMessage message = new MqttMessage();
+        message.setPayload("bá".getBytes());
+        try {
+            client.publish("kula", message);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
