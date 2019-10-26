f22ee9cef21d104818eb52206de192393226c5e2
Tóth Martin <tothmartinofficial@gmail.com>
2019-10-26 08:19:46 +0200

    Timetable is in da house

--- app/src/main/java/com/example/myapplication/CheckActivity.java
+++ app/src/main/java/com/example/myapplication/CheckActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.tlaabs.timetableview.Schedule;
+import com.github.tlaabs.timetableview.Time;
+import com.github.tlaabs.timetableview.TimetableView;
+
+import java.util.ArrayList;
+
 public class CheckActivity extends AppCompatActivity {
+    TimetableView timetable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check);
+
+        timetable = findViewById(R.id.timetable);
+        timetable.setHeaderHighlight(2);
+
+        ArrayList<Schedule> schedules = new ArrayList<Schedule>();
+        Schedule schedule = new Schedule();
+        schedule.setClassTitle("Data Structure"); // sets subject
+        schedule.setClassPlace("IT-601"); // sets place
+        schedule.setProfessorName("Won Kim"); // sets professor
+        schedule.setStartTime(new Time(10,0)); // sets the beginning of class time (hour,minute)
+        schedule.setEndTime(new Time(13,30)); // sets the end of class time (hour,minute)
+        schedules.add(schedule);
+//.. add one or more schedules
+        timetable.add(schedules);
     }
+
+
 }
