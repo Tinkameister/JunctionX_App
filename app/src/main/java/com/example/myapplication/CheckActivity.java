6c1751d30ecc0330fa13e061c83ef57320f9b941
dugaszadam <dugasz.adam@gmail.com>
2019-10-27 03:16:12 +0100

    Calendar left-right

--- app/src/main/java/com/example/myapplication/CheckActivity.java
+++ app/src/main/java/com/example/myapplication/CheckActivity.java
@@ -1,8 +1,8 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.tlaabs.timetableview.Schedule;
 import com.github.tlaabs.timetableview.Time;
@@ -10,8 +10,13 @@ import com.github.tlaabs.timetableview.TimetableView;
 
 import java.util.ArrayList;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class CheckActivity extends AppCompatActivity {
     TimetableView timetable;
+
+    Button s, c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,25 @@ public class CheckActivity extends AppCompatActivity {
         schedule.setEndTime(new Time(13,30)); // sets the end of class time (hour,minute)
         schedules.add(schedule);
         timetable.add(schedules);
+
+        s = findViewById(R.id.leftButton);
+        s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ez lesz az adott hét mínusz 604800
+            }
+        });
+
+        c = findViewById(R.id.rightButton);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ez lesz az adott hét plusz 604800
+            }
+        });
     }
 
 
+
 }
+
