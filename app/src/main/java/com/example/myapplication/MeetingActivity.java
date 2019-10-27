48e138d16a9fb9ac00a1f9d71564dd0fa879bb29
dugaszadam <dugasz.adam@gmail.com>
2019-10-27 07:28:32 +0100

    Update

--- app/src/main/java/com/example/myapplication/MeetingActivity.java
+++ app/src/main/java/com/example/myapplication/MeetingActivity.java
@@ -1,11 +1,6 @@
 package com.example.myapplication;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,28 +10,6 @@ public class MeetingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeting);
-    }
-
-    if(int a=8){
-        int i=0;
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-        Button btn = new Button(this);
-        btn.setId(i);
-        i++;
-        final int id_ = btn.getId();
-        btn.setText("IDE");                                        //----->ide jön a foglalt szobaszám és az időpont(dátum és kezdő idő)
-        btn.setBackgroundColor(Color.rgb(70, 80, 90));
-        linear.addView(btn, params);
-        btn1 = ((Button) findViewById(id_));
-        btn1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(),
-                        "Button clicked index = " + id_, Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
     }
 
 
